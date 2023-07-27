ca731ac81d1fa058e224a710bdfc7f89a055ecef
Netology test <wice.tiger@yandex.ru>
2023-07-27 23:53:33 +0400

    Homework2

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -15,6 +15,16 @@ class ProfileViewController: UIViewController {
         return profileHeaderView
     }()
     
+    private let newButton: UIButton = {
+        let view = UIButton()
+        view.backgroundColor = UIColor.green
+        view.setTitle("Кнопка снизу", for: .normal)
+        view.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        view.layer.masksToBounds = false
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .lightGray
@@ -24,12 +34,17 @@ class ProfileViewController: UIViewController {
     }
     func setupUI(){
         view.addSubview(profileHeaderView)
-
+        view.addSubview(newButton)
+        
         NSLayoutConstraint.activate([
             profileHeaderView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             profileHeaderView.heightAnchor.constraint(equalToConstant: 220),
             profileHeaderView.leftAnchor.constraint(equalTo: view.leftAnchor),
             profileHeaderView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            
+            newButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            newButton.leftAnchor.constraint(equalTo: view.leftAnchor),
+            newButton.rightAnchor.constraint(equalTo: view.rightAnchor)
         ])
         
     }
